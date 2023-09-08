4ad1f2617d4417a72cfe533e17ecdfedf936df08
Sabriwwe <126092940+Sabriwwe@users.noreply.github.com>
2023-09-08 15:40:21 +0300

    Create lab_3.c

---
+++ unit_2/lesson_1/lab_3.c
@@ -0,0 +1,25 @@
+/*
+name ; sabri elenani
+lab_3 coding .
+*/
+
+#include <stdio.h>
+
+int main() {
+    
+      int a ,b;
+      printf("enter two nums : ");
+      fflush(stdin); fflush(stdout);
+      scanf ("%d",&a);
+      scanf ("%d",&b);
+      
+      if (a < b )
+      {
+          printf("the minimum = %d ", a);
+      }
+      else
+      {
+          printf("the minimum = %d ", b);
+      }
+      
+    }
