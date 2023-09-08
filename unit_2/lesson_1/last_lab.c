e8f21ecad1eebb847f73c5a10b1fccfd684b573d
Sabriwwe <126092940+Sabriwwe@users.noreply.github.com>
2023-09-08 16:13:59 +0300

    Create last_lab.c

---
+++ unit_2/lesson_1/last_lab.c
@@ -0,0 +1,33 @@
+/*
+name : sabri elenani .
+last_lab_in_lesson1 coding .
+*/
+
+#include <stdio.h>
+
+int main() {
+    
+     // printing the form
+    /*
+    0123456789
+    123456789
+    23456789
+    3456789
+    456789
+    56789
+    6789
+    789
+    89
+    9
+    */
+    
+    for(int i =0 ; i<=9; i++){
+    for(int j =i ;j<=9 ;j++){
+        printf("%d",j);
+    }
+        printf("\n");
+    }
+    
+    
+    
+}
