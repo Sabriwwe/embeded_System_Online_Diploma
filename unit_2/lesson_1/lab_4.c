cdef95f24e8d0a57c4a9249967277080a67a4017
Sabriwwe <126092940+Sabriwwe@users.noreply.github.com>
2023-09-08 15:47:22 +0300

    Create lab_4.c

---
+++ unit_2/lesson_1/lab_4.c
@@ -0,0 +1,17 @@
+/*
+name : sabri elenani .
+lab_4 coding .
+*/
+
+#include <stdio.h>
+
+int main() {
+    
+      int a = 0 , i =0 ;
+      
+      for (i ; i<=99; i++)
+      {
+          a += i;
+      }
+      printf("summation of values between (1 & 99) = %d ",a);
+    }
